1e6e814ce02fe0322d911c1209fee67a5a6c5600
wineshuga <uziwinnie@gmail.com>
2022-11-11 15:54:08 +0100

    refactor: upload and rename images feat: create components, assets and page folders feat: create card, navbar and footer components feat: install and use router-dom

--- metabnb/src/App.js
+++ metabnb/src/App.js
@@ -1,8 +1,16 @@
 import './App.css';
+import Home from "./Pages/Home"
+import Place from './Pages/Place';
+import {Routes, Route} from "react-router-dom";
 
 function App() {
   return (
-    <></>
+    <>
+      <Routes>
+        <Route path={Home}>Home</Route>
+        <Route path={Place}>Place to stay</Route>
+      </Routes>
+    </>
   );
 }
 
---
+++ metabnb/src/Components/Navbar.js
@@ -0,0 +1,9 @@
+import Logo from "../Assets/Logo"
+
+export default function Navbar() {
+    return (
+        <nav>
+
+        </nav>
+    )
+}
