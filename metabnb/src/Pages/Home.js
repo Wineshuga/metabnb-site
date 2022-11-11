243a275cabc750aad83bae710743674612e0fe90
wineshuga <uziwinnie@gmail.com>
2022-11-12 00:53:22 +0100

    refactor: add font weight 700 link to index.html style: hero section styling

--- metabnb/src/Pages/Home.js
+++ metabnb/src/Pages/Home.js
@@ -1,9 +1,22 @@
 import Navbar from "../Components/Navbar";
+import HeroImg from "../Assets/Homepage1.png"
 
 export default function Home() {
     return (
-        <div>
-        <Navbar />
+        <div className="home">
+            <Navbar />
+            <main>
+                <section className="home--hero">
+                    <div className="home--hero_text">
+                        <h1>Rent a <span className="purple">Place</span> away from <span className="purple">Home</span> in the <span className="purple">Metaverse</span></h1>
+                        <p>we provide you access to luxury and affordable houses in the metaverse,
+                            get a chance to turn your imagination to reality at your comfort zone</p>
+                        <input className="home--hero_searchbox" type={'search'} placeholder="Search for location" />
+                        <input type={'submit'} value="Search" className="button searchbox" />
+                    </div>
+                    <img src={HeroImg} alt="" />
+                </section>
+            </main>
         </div>
     )
 }
