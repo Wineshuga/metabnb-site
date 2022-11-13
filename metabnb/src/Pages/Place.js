a5738b9dfeb6933c1a5c0295a24f9098715291a4
wineshuga <uziwinnie@gmail.com>
2022-11-13 19:21:18 +0100

    feat: create place-to-stay page and component style: add navbar, footer and second navbar

--- metabnb/src/Pages/Place.js
+++ metabnb/src/Pages/Place.js
@@ -1,6 +1,25 @@
+import Navbar from "../Components/Navbar"
+import Footer from "../Components/Footer"
+import Setting from "../Assets/setting-5.png"
+
 export default function Place() {
     return (
-        <>
-        </>
+        <div className="place">
+            <Navbar />
+            <div className="place--second_nav">
+                <ul>
+                    <li>Resturant</li>
+                    <li>Cottage</li>
+                    <li>Castle</li>
+                    <li>fantast city</li>
+                    <li>beach</li>
+                    <li>Carbins</li>
+                    <li>Off-grid</li>
+                    <li>Farm</li>
+                </ul>
+                <button className="place--location_btn">Location <img src={Setting} alt="" /></button>
+            </div>
+            <Footer />
+        </div>
     )
 }
